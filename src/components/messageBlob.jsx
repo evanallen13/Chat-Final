d621b845910ddcbb41d5634ff1a3bfbe0db92976
Evan Allen <evanallen13@gmail.com>
2019-10-02 13:02:42 -0700

    add textblobs

--- src/components/messageBlob.jsx
+++ src/components/messageBlob.jsx
@@ -5,31 +5,67 @@ import * as firebase from 'firebase';
 const MessageBlob = (props) =>{
     return(
         ( props.User === firebase.auth().currentUser.displayName ?
-            <BlobUser>{props.User} : {props.Message}</BlobUser> :
-            <BlobNonUser>{props.User} : {props.Message}</BlobNonUser>
+            <UserMessage>
+                <h4>{getDate(props.Time)}</h4>
+                <BlobUser>
+                    {props.Message}
+                </BlobUser>
+            </UserMessage> :
+            <NonUserMessage>
+                <h4>{props.User} {props.Time}</h4>
+                <BlobNonUser>{props.Message}</BlobNonUser>
+            </NonUserMessage>
         )
     )
 }
+const getDate = (time) =>{
+    const day = new Date()
+    console.log(time)
+
+    return time
+}
+
+const UserMessage = styled.div`
+    float : right;
+    margin : 5px 5px 5px 5px;
+    color : white;
+    text-align : start;
+    h4{
+        margin : 0;
+        margin-left : 10px;
+        font-size : 15px;
+    }
+`
 
 const BlobUser = styled.div`
     background-color : 	#5fc9f8;
     color : white;
-    margin : 5px 5px 5px 5px;
     border-radius : 5px;
     padding : 5px 5px 5px 5px
-    width : 50%;
-    float : left;
+    width : 50vw;
+    max-width : 390px;
     overflow : wrap;
     overflow-wrap : break-word;
 `
-const BlobNonUser = styled.div`
-    background-color : 	#53d769;
-    color : white;
+const NonUserMessage = styled.div`
+    float : left;
+    width : 50%
     margin : 5px 5px 5px 5px;
+    color : white;
+    text-align : start;
+    h4{
+        margin : 0;
+        margin-left : 10px;
+        font-size : 15px;
+    }
+`
+const BlobNonUser = styled.div`
+    background-color : 	#F5F5F5;
+    color : black;
     border-radius : 5px;
     padding : 5px 5px 5px 5px
-    width : 50%;
-    float : right;
+    width : 50vw;
+    max-width : 390px;
     overflow : wrap;
     overflow-wrap : break-word;
 `
